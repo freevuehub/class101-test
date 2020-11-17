5ad11f4aec2e9f63d99b864559d7b708b268c4c2
freevuehub <hackish113@gmail.com>
2020-11-17 14:56:48 +0900

    [improved]: type 추가

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, Store } from 'redux'
 import rootReducer from './store'
 import App from './App'
+import { IStoreState } from './types'
 
 import 'antd/dist/antd.css'
 
-const store = createStore(rootReducer)
+const store: Store<IStoreState> = createStore(rootReducer)
 const root: HTMLElement | null = document.getElementById('root')
 
 ReactDOM.render(
